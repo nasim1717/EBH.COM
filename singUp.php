9b8e60e5ea79aeefc59951a7335af70db391a4e1
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2022-06-25 20:28:42 +0600

    Update singUp.php

--- singUp.php
+++ singUp.php
@@ -1,3 +1,14 @@
+<?php
+    $hostname = 'localhost';
+    $username = 'root';
+    $pas = '';
+    $dbms_name = 'ebh';
+    $conn = new mysqli($hostname,$username,$pas,$dbms_name);
+    if($conn->connect_errno){
+        die("Connection failed:". $conn->connect_errno);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,35 +16,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/main.css">
-    <title>EBH.com</title>
+    <title>Sing Up</title>
 </head>
 <body>
+<?php
+        if(isset($_POST['number'])){
+            $num = $_POST['number'];
+            $mail = $_POST['email'];
+            $pass = $_POST['password'];
+            if(!empty($num) && !empty($pass) && !empty($mail)){    //// if(!empty($num) && !empty($pass) && !empty($mail)) any one code
+                $len = strlen($num);
+                $sql = "INSERT INTO registration(password,number,email)
+                        VALUES('$pass','$num','$mail')";
+                if($len==11){
+                    if($conn->query($sql) == TRUE){
+                        echo 'informatiion inserted';      
+                    }
+                    // alredy gmail and phone number ace kina check..
+                    else {
+                        $sql = "SELECT number from registration WHERE number=$num";
+                        $cheking_row = mysqli_query($conn,$sql);
+                        $num_row = mysqli_num_rows($cheking_row);
+                        if($num_row==TRUE)
+                            echo 'number alredy cretae '."<br>"; /// pore chenge korte hobe
+                        $sql = "SELECT email from registration WHERE email='$mail'";
+                        $cheking_row = mysqli_query($conn,$sql);
+                        $num_row = mysqli_num_rows($cheking_row);
+                        if($num_row==TRUE)
+                            echo "gmail alredy create";
+                    } 
+                }
+                elseif($len<11){
+                    $digit = 11;
+                    echo "number digit kom ",$digit-$len; /// english  likhte hobe english ta sajed likhe debe
+                }
+                elseif($len>11){
+                    $digit = 11;
+                    echo  "number digit beshi ",$len-$digit; ///english likhte hobe..
+                }
+                
+            }
+            else{
+               echo "Please enter the information";
+
+            }
+        }
+    ?>
     <section>
-        <div class="title-div">
-            <h2>এবার বিয়ে হবে</h2>
+        <h2 style="position: absolute; transform: translate(-50%,-50%); left: 50%;top: 10vh;" class="singup-titel">আপনার হিসাব খুলুন</h2>
+        <div class="singup-div">
+            <form action="singUp.php" method="POST">
+                <!-- <input placeholder="ব্যবহারকারীর নাম" type="text"> -->
+                <input placeholder="ফোন নম্বর" type="text" name="number">
+                <input placeholder="বৈদ্যুতিক মেইল" type="email" name="email">
+                <input placeholder="গুপ্তমন্ত্র" type="password" name="password">
+                <!-- <input placeholder="পেশা" type="text"> -->
+                <!-- <input placeholder="ধর্মবিশ্বাস" type="text"> -->
+                <input class="sub-btn" value="দাখিল" type="submit">
+            </form>
         </div>
-        
-        <div class="button_section">
-            <img src="./img/Fancy-Wedding-Border-PNG-Image.webp" alt="">
-        </div>
-        <div class="coves_img">
+        <div class="nav-bar">
+            <a href="./index.php"><button>home</button></a>
 
+            <a href="./login.php"><button>login</button></a>
         </div>
-        <div class="buttons">
-            <a href="./login.php">
-                <button>প্রবেশ করুন</button>
-            </a>
-            <a href="./singUp.php">
-                <button>তালিকাভুক্ত করুন</button>
-            </a>
-        </div>
-
-        <img class="wedding-bell" src="./img/wedding-bell-silhouette-1.webp" alt="">
-        <!-- <div class="wedding-bell">
-        </div> -->
-        <!-- <div>
-            <img src="./img/wedding_PNG19475.webp" alt="">
-        </div> -->
     </section>
 </body>
 </html>
